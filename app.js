ab4ab055ff4d5cb0e9b0a1f855ff82b2fe980832
Michael Campbell <cam3000@gmail.com>
2020-09-12 00:18:15 -0700

    js didnt work at first I cut it out then pasted back in for some reasone it sometimes work this time it did

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-'use strict';
+// DOM Elements
 
-// Dom Elements
- const darkButton = document.getElementById('dark');
- const LightButton = document.getElementById('light');
- const solarButton = document.getElementById('solar');
- const body = document.body;
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const solarButton = document.getElementById('solar');
+const body = document.body;
 
- // Apply the cached theme on reload
 
- const theme = localStorage.getItem('theme');
- const isSolar = localStorage.getItem('isSolar');
+// Apply the cached theme on reload
 
- if (theme) {
-   body.classList.add(theme);
-   isSolar && body.classList.add('solar');
- }
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
 
- // Button Event Handlers
+if (theme) {
+  body.classList.add(theme);
+  isSolar && body.classList.add('solar');
+}
 
- darkButton.onclick = () => {
-   body.classList.replace('light', 'dark');
-   localStorage.setItem('theme', 'dark');
- };
+// Button Event Handlers
 
- LightButton.onclick = () => {
-   body.classList.replace('dark', 'light');
+darkButton.onclick = () => {
+  body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
+};
 
-   localStorage.setItem('theme', 'light');
- };
+lightButton.onclick = () => {
+  body.classList.replace('dark', 'light');
 
- solarButton.onclick = () => {
+  localStorage.setItem('theme', 'light');
+};
+
+solarButton.onclick = () => {
 
   if (body.classList.contains('solar')) {
+    
     body.classList.remove('solar');
-    solarButton.style.cssText = 
-    --bg-solar: var(--yellow);
+    solarButton.style.cssText = `
+      --bg-solar: var(--yellow);
+    `
 
     solarButton.innerText = 'solarize';
-    localStorage.removeItem('isSolar');
-  }
-  else{
 
-    solarButton.style,cssText =
-    --bg-solar: white;
+    localStorage.removeItem('isSolar');
+
+  } else {
+
+    solarButton.style.cssText = `
+      --bg-solar: white;
+    `
 
     body.classList.add('solar');
     solarButton.innerText = 'normalize';
 
     localStorage.setItem('isSolar', true);
-
   }
-}
+};
  
