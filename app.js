a61b776e7cef55e88ff61ca503cc5a7190885416
Michael Campbell <cam3000@gmail.com>
2020-09-11 23:54:15 -0700

    added dom elements and local storage

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Dom Elements
+ const darkButton = document.getElementById('dark');
+ const LightButton = document.getElementById('light');
+ const solarButton = document.getElementById('solar');
+ const body = document.body;
+
+ // Apply the cached theme on reload
+
+ const theme = localStorage.getItem('theme');
+ const isSolar = localStorage.getItem('isSolar');
+
+ if (theme) {
+   body.classList.add(theme);
+   isSolar && body.classList.add('solar');
+ }
+
+ // Button Event Handlers
+
+ 
